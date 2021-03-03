b182b966b8ede959702047927387950589cd9d50
Mamadou NDONGO <ndong003@gmail.com>
2021-03-03 01:34:44 +0100

    finalisation html

--- Desktop/P5_NDONGO_MAMADOU/P5/FRONTEND/js/fonction-float.js
+++
@@ -1,6 +0,0 @@
-function convertToFloatNumber(number) {
-    number = (number / 100).toFixed(2);
-    return number;
-};
-
-export { convertToFloatNumber };
--- Desktop/P5_NDONGO_MAMADOU/P5/FRONTEND/js/index.js
+++ Desktop/P5_NDONGO_MAMADOU/P5/FRONTEND/js/index.js
@@ -1,42 +1,63 @@
-import { convertToFloatNumber } from "./fonction-float.js";
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
 
-let cardsList = document.getElementById("cards__list");
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
+  }
+});
 
-fetch("http://localhost:3000/api/cameras/")
-    .then(response => response.json()) // Return an JavaScript object
-    .then(response => {
-        for (let i in response) { // For each camera contained in the JSON object (no matter how many)
-            let newDiv = document.createElement("div"); // Create new card, add class .card and put it in cards__list div
-            newDiv.classList.add("card");
-            cardsList.appendChild(newDiv);
-            let newLink = document.createElement("a"); // Insert in the div link, img product, description with name & price product
-            newLink.setAttribute("href", "produit.html?id=" + response[i]["_id"]);
-            newLink.classList.add("card__link");
-            newDiv.appendChild(newLink);
-            let newImage = document.createElement("img"); 
-            newImage.classList.add("card__image");
-            newLink.appendChild(newImage);
-            let newDescription = document.createElement("div");
-            newDescription.classList.add("card__description")
-            newLink.appendChild(newDescription);
-            let newName = document.createElement("span");
-            newName.classList.add("card__name");
-            newDescription.appendChild(newName);
-            let newPrice = document.createElement("span");
-            newPrice.classList.add("card__price");
-            newDescription.appendChild(newPrice);
-            fetch("http://localhost:3000/api/cameras/" + response[i]["_id"]) // Call elements dependings on url
-            .then(response => response.json())
-            .then(response => {
-                newImage.src = response["imageUrl"]; // Take imageUrl of response for the img src
-                newName.textContent = response["name"]; // Display reponse name
-                newPrice.textContent = convertToFloatNumber(response["price"]) + "€"; // Display reponse price with 2 decimals
-            })
-            .catch(error => console.log(error));
-        }
-    })
-    .catch(error => {
-        console.log(error);
-        alert("Erreur de serveur, tentative de reconnnexion...");
-        setTimeout(function(){document.location.reload()}, 1000);
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
+});
+
+// Fixed Nav
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  if (!link) return;
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
     });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
